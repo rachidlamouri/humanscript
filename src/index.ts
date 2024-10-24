7e525410a16fa583a37ff5321fefe579cdf09796
Rachid Lamouri <rachidlamouri@gmail.com>
2024-10-23 21:26:38 -0400

    Support compiling all levels at once

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,40 @@
 import fs from 'fs';
 import { compile } from './compiler/compiler';
 import { assertIsNotUndefined } from './utils/assertIsNotUndefined';
+import { parseArgs } from 'util';
+import { posix } from 'path';
 
-const [inputFilePath] = process.argv.slice(2);
-assertIsNotUndefined(inputFilePath);
+const {
+  positionals: [inputFilePath],
+  values: { levels: compileLevels = false },
+} = parseArgs({
+  args: process.argv.slice(2),
+  allowPositionals: true,
+  options: {
+    levels: { type: 'boolean' },
+  },
+});
 
-const inputCode = fs.readFileSync(inputFilePath, 'utf8');
-const outputCode = compile(inputCode);
+let filePaths: string[];
 
-const outputFilePath = inputFilePath + 'c';
+if (compileLevels) {
+  const directory = './programs';
+  filePaths = fs
+    .readdirSync(directory)
+    .filter((file) => !file.endsWith('c'))
+    .map((file) => posix.join(directory, file));
+} else {
+  assertIsNotUndefined(inputFilePath);
+  filePaths = [inputFilePath];
+}
 
-fs.writeFileSync(outputFilePath, outputCode);
+filePaths.forEach((filePath) => {
+  console.log(`Compiling: ${filePath}`);
+  const inputCode = fs.readFileSync(filePath, 'utf8');
+  const outputCode = compile(inputCode);
 
-console.log(`Compiled: ${outputFilePath}`);
+  const outputFilePath = filePath + 'c';
+  fs.writeFileSync(outputFilePath, outputCode);
+  console.log(`Compiled: ${outputFilePath}`);
+  console.log();
+});
